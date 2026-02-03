8a11d93cf223868c724e5223ea93823d3a025849
Ilya Gorodnichev <ilya.gorodnichev.04@mail.ru>
2026-02-04 01:03:37 +0500

    Создан класс для вывода в консоль

---
+++ src/main/java/com/example/Service/ConsolePrintService.java
@@ -0,0 +1,12 @@
+package com.example.Service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsolePrintService implements PrintService {
+
+    @Override
+    public void print(String message) {
+
+    }
+}
