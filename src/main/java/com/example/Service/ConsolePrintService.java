d12dfdcb4691d3b0ad5b4487c852acb364832f4c
Ilya Gorodnichev <ilya.gorodnichev.04@mail.ru>
2026-02-04 01:22:32 +0500

    Добавлены префиксы и сам вывод

--- src/main/java/com/example/Service/ConsolePrintService.java
+++ src/main/java/com/example/Service/ConsolePrintService.java
@@ -1,12 +1,16 @@
 package com.example.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConsolePrintService implements PrintService {
 
+    @Value("${print.console.prefix}")
+    private String prefix;
+
     @Override
     public void print(String message) {
-
+        System.out.println(prefix + " " + message);
     }
 }
--- src/main/java/com/example/Service/FilePrintService.java
+++ src/main/java/com/example/Service/FilePrintService.java
@@ -1,12 +1,16 @@
 package com.example.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FilePrintService implements PrintService {
 
+    @Value("${print.file.prefix}")
+    private String prefix;
+
     @Override
     public void print(String message) {
-
+        System.out.println(prefix + " " + message);
     }
 }
