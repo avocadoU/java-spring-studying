c237fd416bdff4a67c3fcf5274a1a93c77dffcd2
Ilya Gorodnichev <ilya.gorodnichev.04@mail.ru>
2026-02-04 02:27:18 +0500

    Обновлен конфиг для планировщика

--- src/main/java/com/example/Config.java
+++ src/main/java/com/example/Config.java
@@ -3,9 +3,11 @@ package com.example;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @ComponentScan
+@EnableScheduling
 @PropertySource("classpath:application.properties")
 public class Config {
 }
