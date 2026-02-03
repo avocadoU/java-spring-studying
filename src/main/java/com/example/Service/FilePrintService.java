52929417a61ee5214af13dca8d4032715e441073
Ilya Gorodnichev <ilya.gorodnichev.04@mail.ru>
2026-02-04 01:10:37 +0500

    Создан класс для вывода в файл (условно)

---
+++ src/main/java/com/example/Service/FilePrintService.java
@@ -0,0 +1,12 @@
+package com.example.Service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FilePrintService implements PrintService {
+
+    @Override
+    public void print(String message) {
+
+    }
+}
