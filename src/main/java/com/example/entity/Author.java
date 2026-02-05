0946a468da0166d746677600b6869ad7d0e4dd01
Ilya Gorodnichev <ilya.gorodnichev.04@mail.ru>
2026-02-05 22:07:06 +0500

    Реализован класс Author

--- src/main/java/com/example/entity/Author.java
+++ src/main/java/com/example/entity/Author.java
@@ -1,4 +1,70 @@
 package com.example.entity;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_author")
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books = new ArrayList<>();
+
+    public Author() {
+
+    }
+
+    public Author(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
