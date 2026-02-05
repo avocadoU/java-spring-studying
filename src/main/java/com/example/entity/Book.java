b903befe5ad03262bde912b1980591f7b26cae3c
Ilya Gorodnichev <ilya.gorodnichev.04@mail.ru>
2026-02-05 22:00:16 +0500

    Реализован класс Book

--- src/main/java/com/example/entity/Book.java
+++ src/main/java/com/example/entity/Book.java
@@ -1,4 +1,71 @@
 package com.example.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_book")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Book() {
+
+    }
+
+    public Book(String title, Author author, User user) {
+        this.title = title;
+        this.author = author;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author=" + author +
+                ", user=" + user +
+                '}';
+    }
 }
